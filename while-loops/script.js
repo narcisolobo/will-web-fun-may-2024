a72f64b6e761cdd33a6de766b7046eb85b0533bb
narcisolobo <narcisolobo@gmail.com>
2024-06-11 21:49:52 -0700

    initial commit

---
+++ while-loops/script.js
@@ -0,0 +1,18 @@
+// WHILE LOOPS
+// FOR LOOPS THAT ARE "DECONSTRUCTED"
+
+/*
+  iterator (i)
+  iterator start
+  iterator end ("as long as")
+  iterator changes
+*/
+
+let i = 0;
+while (i <= 10) {
+  console.log(i);
+  i++;
+}
+
+// A for loop is for when you know how many times a loop should run
+// A while loop is for when you do not
